89ddb679cb6240f7bbedded858e8bcb14dc2037f
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-08 13:35:36 +0300

    Setup Components Files

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+
+import { configureStore  } from "@reduxjs/toolkit";
+// import  counterSlice  from "./slices/counter";
+
+export default configureStore({
+    reducer: {
+        // counter:counterSlice,
+     },
+})
+
+/**Comments As Example only */
