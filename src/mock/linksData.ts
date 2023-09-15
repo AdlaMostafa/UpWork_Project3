e4e8f2bd675cfe3dea73b2a5087d7993d08ffe9f
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-16 00:12:05 +0300

    Mock data file for Links the lase Section in Right Part in Home Page

---
+++ src/mock/linksData.ts
@@ -0,0 +1,26 @@
+export interface linkItem{
+    id: number;
+    name: string;
+}
+export const linkData:linkItem[] =[
+    {
+        id: 1,
+        name:'Upwork Academy',
+    },
+    {
+        id: 2,
+        name:'Direct Contracts',
+    },
+    {
+        id: 3,
+        name:'Get Paid',
+    },
+    {
+        id: 4,
+        name:'Community & Forums',
+    },
+    {
+        id: 5,
+        name:'Help Center',
+    },
+]
