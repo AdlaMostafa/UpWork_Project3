1daa1e5dc66f0f40b8aeaf74663cc17733d8537a
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-16 01:46:08 +0300

    Style for Link  Section

---
+++ src/components/pages/HomePage/sections/rightSections/Links/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledLinksSection = styled.div`
+border-radius: 20px;
+margin-top: 20px;
+border: 1px solid lightgray;
+font-family: 'PP Neue Montreal', sans-serif;
+a{
+    margin: 10px;
+    text-decoration-color: black;
+}
+
+`
