5aa99927ea184aa0db7c967fa5f0be5a54198ee5
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-15 23:47:45 +0300

    Style for Header Component

---
+++ src/components/organism/Header/style.ts
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.div`
+background-color: white;
+margin-top: -20px;
+width: 100%;
+position: relative;
+display: flex;
+align-items: center;
+justify-content: space-between;
+img{
+    margin-top: 14px;
+    margin-right: 10px;
+}
+a{
+    text-decoration: none;
+    color :grey;
+    margin-top: 14px;
+    margin-left: 10px;
+    font-size: 20px;
+}
+`
+export const StyledSearch = styled.div`
+border: none;
+margin-top: 20px;
+`
+export const StyleQueIcon = styled.div`
+    margin-top: 20px;
+    margin-left: 5px;
+    margin-right: 10px;
+`
+export const StyleSendIcon = styled.div`
+    margin-top: 20px;
+    margin-left: 5px;
+    margin-right: 10px;
+`
+export const StyleBellIcon = styled.div`
+    margin-top: 20px;
+    margin-left: 5px;
+    margin-right: 10px;
+`
