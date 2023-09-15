00fb2565cfe2cc5e4e3d1393b8335ef91aa5bad5
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-16 01:26:36 +0300

    Style for Hero Section

---
+++ src/components/pages/HomePage/sections/leftSections/HeroSection/style.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const StyledHero = styled.div`
+background-color: #1f57c3;
+display: flex;
+align-items:center;
+justify-content:space-between;
+width:100%;
+font-family: 'PP Neue Montreal', sans-serif;                              
+color: white;
+font-size: 15px;
+border-radius: 30px;
+margin-bottom: 20px;
+.leftContent{
+    margin-left: 30px;
+}
+h1{
+    font-weight: 450;
+}
+img{
+    width: 160px;
+    height: 120px;
+    margin-right: 15px;
+}
+`
