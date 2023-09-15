5c6a1354ba7cfcdb732e37d6a278d2233656965c
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-16 00:55:43 +0300

    Code for Login Form

---
+++ src/components/pages/Auth/LoginPage/LoginForm/style.ts
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+                
+export const StyledTitleForm = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+h2{
+    font-family: 'PP Neue Montreal', sans-serif;                                               
+}
+`
+export const StyledParentForm = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+margin-top: 5px;
+margin-bottom:31px;
+`
+export const StyledLoginForm = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+border: 1px solid black;
+width: 480px;
+`
+
+export const StyledButtonsParent = styled.div`
+display: flex;
+align-items: center;
+flex-direction: column;
+margin-bottom: 20px;
+button{
+    margin-bottom: 20px;
+    margin-top: 10px;
+}
+`
+export const StyleGoogleButton=styled.div`
+img{
+    border: 1px solid white;
+    background-color: white;
+    border-radius: 50%;
+    margin-left: -88px;
+    margin-right: 84px;
+    margin-top: -5px;
+}
+`
+export const StyleAppleButton=styled.div`
+img{
+    margin-left: -6px;
+    margin-right: 6px;
+    margin-top: 9;
+}
+`
+export const StyleSignupButton=styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+margin-top: 20px;
+`
