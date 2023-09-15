7d213e9079ae5ab83341e2e13e7b3cc5635d9c77
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-16 01:07:52 +0300

    Password componenet for SignUp Form

---
+++ src/components/pages/Auth/SignupPage/SignupForm/SignupComponent/Password/index.tsx
@@ -0,0 +1,52 @@
+'use client';
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import Input from '@mui/material/Input';
+import FilledInput from '@mui/material/FilledInput';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import InputLabel from '@mui/material/InputLabel';
+
+export default function PasswordInput() {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <FormControl variant="standard" sx={{ m: 1,
+         width: '560px',
+         height:"40px",
+         border:"1px solid Gray",
+         borderRadius:"10px" }}>
+        <Input
+        placeholder="Password (8 or more character)"
+        disableUnderline
+        sx={{ 
+          marginLeft:'23px',
+          marginTop:'5px'
+         }}
+          type={showPassword ? 'text' : 'password'}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+    </div>
+  );
+}
