d70bd8d6daf6d40fc35f9f30ac2445b69fea4eff
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-16 00:09:58 +0300

    Mock data file for jobDetails the Main Section in Left Part For Home Page that in Tabs

---
+++ src/mock/jobData.ts
@@ -0,0 +1,115 @@
+export interface JobDetails {
+    id: number;
+    title: string;
+    time: string;
+    description: string;
+    skills: string[];
+    propsalTime: [string, string];
+    price: [string, string, string, string];
+  }
+
+export const jobDetails = [
+{
+    id:1,
+    title:'Get HTML,CSS,JS with bootstrap',
+    time:'Fiexed-price -Intermediate-Est:$30 - Posted 6 hours ago',
+    description:'rewrite the page https://www.topcv.vn/ with bootstraps.(reset classes,ids,...)including all effects',
+    skills:['Bootstrp','CSS','HTML','Javascript','jQuery','HTML5','React','next','CSS5','HTML'],
+    propsalTime:['Proposal',':Less than5'],
+    price:['$600','$400','$50','$100'],
+}
+,
+{
+    id:2,
+    title:'Front-End Web Developer for E-commerce Website Redesign',
+    time:'Fixed-price - Expert - Est. Budget: $5 - Posted in 34 minutes',
+    description:'We are in search of a skilled and creative Front-End Web Developer to lead the redesign of our e-commerce website. This project offers an exciting opportunity to revamp our online presence and create an engaging user experience. If youre passionate about web development and have a strong portfolio of front-end projects, we want to hear from you',
+    skills:['Web Developmeent','Web Design','Word Press','HTML','jQuery','HTML5','redux'],
+    propsalTime:['Proposal',':20 to 50'],
+    price:['$600','$400','$50','$100'],
+}
+,
+{
+    id:3,
+    title:'React.js Drag & Drop Widgets / Dashboard',
+    time:'Fixed-price - Intermediate - Est. Budget: $175 - Posted 8 hours ago',
+    description:'Were looking at creating an interactive mock-dashboard (all components will use static data for the purpose of this brief), which enables user to drag-and-drop widgets and to drag-to-move widgets to change layout. Were looking for very beautiful design, matching the colour scheme in example image. Requirements: Please view the attached image for an example of how we would like this to work. ' ,
+    skills:['Web Application','React','HTML','PHP','HTML5'],
+    propsalTime:['Proposal','15 to 20'],
+    price:['$600','$400','$50','$100'],
+}
+,
+{
+    id:4,
+    title:'Front end developer require to create responsive HTML from html',
+    time:'Fiexed-price -Intermediate-Est:$50 - Posted 9 hours ago',
+    description:'I have HTML website pages for desktops/laptops. I am looking for a developer who can create a responsive version of the same html.',
+    skills:['Bootstrp','CSS','HTML','Javascript','jQuery','HTML5'],
+    propsalTime:['Proposal','50+'],
+    price:['$600','$400','$50','$100'],
+}
+,
+{
+    id:5,
+    title:'HI. I need help with a project created with viserbank.',
+    time:'Hourly - Intermediate - Est. Time: Less than 1 month, Less than 30 hrs/week - Posted 9 hours ago',
+    description:'We have integrated a multilevel system on a site built on viserbank. We need to make some fixes to the code because it still doesnt work the way we want',
+    skills:['PSD to HTML','CSS','HTML','Javascript','Web Development','HTML5'],
+    propsalTime:['Proposal','5 to 10'],
+    price:['$600','$400','$50','$100'],
+}
+,
+{
+    id:6,
+    title:'Fix a few small html/css bugs',
+    time:'Fixed-price - Expert - Est. Budget: $5 - Posted 8 hours ago',
+    description:'Fix a few bugs. 1. Our trustpilot widget does not center on screen for mobile version. also on desktop does not center properly. below the order captions now. Fix 2. Set limit to width of column, so if a filename is too long, it will take force it to take more than 1 line to display it. i.e. filename:',
+    skills:['CSS','HTML','CSS3','HTML5'],
+    propsalTime:['Proposal','20 to 50'],
+    price:['$600','$400','$50','$100'],
+}
+,
+ ]
+
+ export const jobDetails2 = [
+    {
+        id:1,
+        title:'Build responsive WordPress site with booking/payment functionality',
+        time:'Hourly: $15-$30 - Intermediate - Est. Time: More than 6 months, Less than 30 hrs/week - Posted in 55 minutes',
+        description:'Ashenafi Wada is a recent computer engineering graduate with a sharp mind. He has excellent communication and cognitive abilities, is adaptive and dynamic, and has the ability to work both independently and in teams. He is also well-versed in computer science and engineering. Good at both front end and back end web development. knowledgeable',
+        skills:['CSS','HTML','PHP','HTML5'],
+        propsalTime:['Proposal',':Less than 4'],
+        price:['$600','$400','$50','$100'],
+    }
+    ,
+    {
+        id:2,
+        title:'Front-End Web Developer for E-commerce Website Redesign',
+        time:'Fixed-price - Expert - Est. Budget: $5 - Posted in 34 minutes',
+        description:'We are in search of a skilled and creative Front-End Web Developer to lead the redesign of our e-commerce website. This project offers an exciting opportunity to revamp our online presence and create an engaging user experience. If youre passionate about web development and have a strong portfolio of front-end projects, we want to hear from you',
+        skills:['Web Developmeent','Web Design','Word Press','HTML','jQuery','HTML5','redux'],
+        propsalTime:['Proposal',':20 to 50'],
+        price:['$600','$400','$50','$100'],
+    }
+    ,
+    {
+        id:3,
+        title:'Image to word file',
+        time:'Fixed-price - Intermediate - Est. Budget: $30 - Posted in 52 minutes',
+        description:'Need someone to retype some image screenshot document, into ms word document' ,
+        skills:['English','Copy writing','Writing','Typing','Data Entry'],
+        propsalTime:['Proposal','Less than 20'],
+        price:['$600','$400','$50','$100'],
+    }
+    ,
+    {
+        id:4,
+        title:'Online Video Editor - MP4Box, Mux.js, FabricJS',
+        time:'Hourly: $15-$25 - Expert - Est. Time: More than 6 months, Less than 30 hrs/week - Posted in 41 minutes',
+        description:'We wanna add video editing features to ssemble.com - Animation (Preset, Keyframe) - Transition (Custom Shader) - And many more Tech stack - Mux.js - MP4 box - Fabric JS - WebGL - React If you have any similar experience on it, please apply for this position.',
+        skills:['FFmpeg','Fabric','Canvas'],
+        propsalTime:['Proposal','50+'],
+        price:['$600','$400','$50','$100'],
+    }
+    ,
+     ]
