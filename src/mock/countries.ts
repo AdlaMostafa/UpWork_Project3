52a8b034cfe1e7a6aa26b0a9093dac6bd171c084
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-16 00:07:37 +0300

    Mock data file for the Select For SignUp page

---
+++ src/mock/countries.ts
@@ -0,0 +1,27 @@
+export interface Countries{
+  id:number;
+  value:string;
+  label:string;
+}
+export const countries:Countries[] = [ 
+  { 
+    id:1,
+    value: "Palestinian Territories", 
+    label: "Palestinian Territories", 
+  }, 
+  { 
+    id:2,
+    value: "America", 
+    label: "America", 
+  }, 
+  { 
+    id:3,
+    value: "Egypt", 
+    label: "Egypt", 
+  }, 
+  { 
+    id:4,
+    value: "Moroco", 
+    label: "Moroco", 
+  }, 
+];
