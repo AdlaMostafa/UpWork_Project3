9292be8a6954e4fa0f3eb91cd1b67e94eb597cef
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-16 00:06:32 +0300

    Mock data file for the Second Section in Right part for the HomePage

---
+++ src/mock/connects.ts
@@ -0,0 +1,34 @@
+export interface Connect {
+    id: string;
+    title: string;
+    about: string;
+  }
+  
+  export const connects: Connect[] = [
+    {
+      id: '1',
+      title: 'Availability Badge',
+      about: 'Available now',
+    },
+    {
+      id: '2',
+      title: 'Hours per week',
+      about: 'More than 30 hrs/week',
+    },
+    {
+      id: '3',
+      title: 'Profile Visibility',
+      about: 'Public',
+    },
+    {
+      id: '4',
+      title: 'Job Preference',
+      about: 'No preference set',
+    },
+    {
+      id: '5',
+      title: 'My Categories',
+      about: 'Web Development',
+    },
+  ];
+  
