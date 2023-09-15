18133fbe8dd8b2a295b79273d3a3ab38cd0b1bac
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-16 01:09:43 +0300

    Style Signup TextFeild componenet for SignUp Form

---
+++ src/components/pages/Auth/SignupPage/SignupForm/SignupComponent/SignupTextFeild/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyleTextFeild = styled.div`
+display: flex;
+align-items: center;
+width: '420px';
+height: 40px;
+border: 1px solid gray;
+border-radius: 10px;
+input{
+    margin-left: 20px;
+    border-bottom: none;
+}
+`
