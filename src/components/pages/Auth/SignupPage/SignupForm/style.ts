c7edc6584518a5d7e2800ddd42c0b9455338c9c1
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-16 01:11:06 +0300

    Style for SignupForm

---
+++ src/components/pages/Auth/SignupPage/SignupForm/style.ts
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+export const StyledTitleForm = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+`
+export const StyledParentForm = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+margin-top: -4px;
+margin-bottom:31px;
+`
+export const StyledSignupForm = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+border: 1px solid black;
+width: 580px;
+`
+
+export const StyledButtonsParent = styled.div`
+display: flex;
+align-items: center;
+flex-direction: column;
+margin-bottom: 20px;
+button{
+    margin-bottom: 20px;
+    margin-top: 10px;
+}
+`
+export const StyleGoogleButton=styled.div`
+img{
+    border: 1px solid white;
+    background-color: white;
+    border-radius: 50%;
+    margin-left: -250px;
+    margin-right: 84px;
+    margin-top: -155px;
+    margin-bottom: -14px;
+}
+`
+export const StyleAppleButton=styled.div`
+img{
+    margin-left: -6px;
+    margin-right: 6px;
+    margin-top: 9;
+}
+`
+
+export const StyledNameFeild = styled.div`
+display: flex;
+align-items:center;
+justify-content: space-around;
+flex-direction: row;
+`
