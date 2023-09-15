1082b8b10efd4e42f14af09a79637a217de491b0
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-15 23:49:50 +0300

    Style for Sub Footer Component

---
+++ src/components/organism/SubFooter/style.ts
@@ -0,0 +1,14 @@
+import { Paper } from "@mui/material";
+import styled from "styled-components";
+
+export const StyledSubFooter = styled(Paper)`
+width: 100%;
+background-color: #001e00;
+border-top: 1px solid white;
+height:70px;
+display: flex;
+align-items: center;
+justify-content: center;
+color: white;
+box-shadow: none;
+`
