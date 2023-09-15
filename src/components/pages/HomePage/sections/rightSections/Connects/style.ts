87004974e6f4d602e8205de2ecdf9fa17f0e0c5c
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-16 01:43:52 +0300

    Style for Connects Section

---
+++ src/components/pages/HomePage/sections/rightSections/Connects/style.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const StyledConnectSection = styled.div`
+margin-top: 20px;
+border-radius: 20px;
+border: 1px solid lightgray;
+font-family: 'PP Neue Montreal', sans-serif;
+.content1{
+    a:hover{
+     text-decoration: underline;
+    }
+    
+}
+`
+export const StyledIconTitle = styled.div`
+        display: flex;
+        align-items: center;
+        justify-content:space-between;
+`
+export const StyledPropsalContent = styled.div`
+display: flex;
+align-items: center;
+flex-direction: column;
+a:hover{
+    text-decoration:underline ;
+}
+`
