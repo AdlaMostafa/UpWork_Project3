e865b018379fadd851c8e4ead403fafbbe261991
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-16 01:35:44 +0300

    Style for Job Div

---
+++ src/components/pages/HomePage/sections/leftSections/Jobs/style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledJobSection = styled.div`
+border: 1px solid black;
+border-radius:10px;
+font-family: 'PP Neue Montreal', sans-serif;
+margin-top: 20px;
+h3{
+    font-size: 21px;
+    margin-left: 20px;
+}
+`
