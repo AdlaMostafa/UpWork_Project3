e5af42b137421c759356f54d5307d170d260f86f
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-15 23:27:20 +0300

    Style code for Avatar component

---
+++ src/components/atoms/Avatar/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import Avatar from '@mui/material/Avatar';
+export const CustomAvatar = styled(Avatar)`
+width: 35px; 
+height: 35px;
+margin-top: 18px;
+margin-left: 5px;
+margin-right: 10px;
+`;
+
+export const StyledAvatar = styled.div`
+margin-top:15px;
+margin-left: 5px;
+`
