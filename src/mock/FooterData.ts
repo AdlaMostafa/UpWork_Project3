0f20ddf832b6dfa8ff883b16ffc0d2669e133f7a
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-16 00:08:14 +0300

    Mock data file for Super Footer

---
+++ src/mock/FooterData.ts
@@ -0,0 +1,22 @@
+export interface FooterItem{
+    id:number;
+    items:string[]
+}
+export const FooterData:FooterItem[]= [
+    {
+        id:1,
+        items:['About us','FeedBack','Community','']
+    },
+    {
+        id:2,
+        items:['Trust,Safety&Security','Help&Support','Upwork Foundation','']
+    },
+    {
+        id:3,
+        items:['Terms of Service','Privacy Policy','CA Notice at Collection','Cookie Settings']
+    },
+    {
+        id:4,
+        items:['Accessibility','Desktop App','Cookie Policy','Enterprise Solution']
+    },
+]
