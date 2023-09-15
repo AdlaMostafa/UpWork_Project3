7711021d7baa58b2dd7f0f6c5d044182e55da468
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-15 23:44:58 +0300

    Style for Like and DisLike Group

---
+++ src/components/molecules/LikeandDislikeGroup/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledLikeDiv = styled.div`
+display: flex;
+align-items: center;
+justify-content: space-around;
+.fav{
+    margin-right: -70px;
+}
+` 
