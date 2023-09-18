a360b05d0baac6cea3feddd367274b0deb4f6262
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-18 10:02:25 +0300

    Mock data file for SceondRightDataSectoin in the RightSection in profilePage

---
+++ src/mock/SecondRightPartData.ts
@@ -0,0 +1,10 @@
+export const SecondRightPartData: {
+    title: string;
+    about: string;
+    link: string;
+  } = {
+    title: 'Work History',
+    about: 'No work yet. Once you start getting hired on Upwork, your work with clients will show up here.',
+    link: 'Start your search',
+  };
+  
