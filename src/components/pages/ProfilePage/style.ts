016e2b5a3299b630897ef3a23a218b66237dd39c
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-18 09:51:47 +0300

    Style code for the ProfilePage

---
+++ src/components/pages/ProfilePage/style.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+
+export const StyleParent = styled.div`
+.add{
+  margin-right: 200px;
+}
+width: 100%;
+margin-top: 20px;
+.section {
+  border: 1px solid lightgray;
+  border-radius: 8px;
+  width: 100%;
+}
+
+
+.left-section {
+  width: 25%;
+  float: left;
+  border-right: 1px solid lightgray;
+}
+
+.right-section {
+  width: 75%;
+  float: left;
+  margin-top: 50px;
+}
+
+.right-part {
+  border: 1px solid lightgray;
+  width: 100%;
+}
+`
+export const StyledSecondPart = styled.div`
+display: flex;
+align-items: center;
+flex-direction: row;
+`
