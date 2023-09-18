3ccf04bb25563c19c31b852e27ea57cc3e27e6e2
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-18 09:37:45 +0300

    Code fir Container For ProfilePage

---
+++ src/components/pages/ProfilePage/components/container/index.tsx
@@ -0,0 +1,29 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import React, { ReactNode } from 'react';
+import {
+  mainContainer,
+  mainContainerPadding,
+  responsiveContainer,
+} from './style'; 
+
+interface ContainerProps {
+  children: ReactNode;
+  noSpaces?: boolean;
+}
+
+const ProfileContainer: React.FC<ContainerProps> = ({ children, noSpaces }) => {
+  return (
+    <div
+      css={[
+        mainContainer,
+        // noSpaces && mainContainerPadding, // Conditionally apply mainContainerPadding
+        responsiveContainer,
+      ]}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default ProfileContainer;
