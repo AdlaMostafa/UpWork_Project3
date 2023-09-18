7f3aa66c7f451d70c240b092940f6e667f832132
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-18 10:00:36 +0300

    Mock data file for FirstSectoin in the RightSection in profilePage

---
+++ src/mock/FirstRightPartData.ts
@@ -0,0 +1,18 @@
+export const FirstRightPartData: {
+  jobTitle: string;
+  hourPrice: string;
+  about: string[];
+  skills: string[];
+  projectManagement: string;
+  communication: string;
+} = {
+  jobTitle: 'FrontEnd developer (React js)',
+  hourPrice: '$10.00/hr',
+  about: [
+    'Adla Alinnabi',
+    'Im a developer experienced in building websites for small and medium-sized businesses. Whether youre trying to win work, list your services, or create a new online store, I can help',
+  ],
+  skills: ['HTML', 'CSS', 'JS', 'Redux', 'Next.js'],
+  projectManagement: 'Full project management from start to finish',
+  communication: 'Regular communication is important to me, so let\'s keep in touch.',
+};
