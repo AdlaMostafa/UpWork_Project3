ebbdf77a3a373349adcb32e14a39f78a074a5c02
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-18 09:59:59 +0300

    Mock data file for FiftheSectoin in the RightSection in profilePage

---
+++ src/mock/FifthRightPartData.ts
@@ -0,0 +1,9 @@
+export const FifthRightPartData: {
+    title: string;
+    about: string;
+  } = {
+    title: 'Your Project Catalog',
+    about:
+      'Projects are a new way to earn on Upwork that helps you do more of the work you love to do. Create project offerings that highlight your strengths and attract more clients.',
+  };
+  
