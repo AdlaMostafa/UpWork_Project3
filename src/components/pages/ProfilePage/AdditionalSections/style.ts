b1c48c95afaab1e92f5672ea366c7aeb52166ecc
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-18 09:35:19 +0300

    Style for AdditionalSections in ProfilePage

---
+++ src/components/pages/ProfilePage/AdditionalSections/style.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const StyledHistoryDiv = styled.div`
+font-family: 'PP Neue Montreal', sans-serif;
+margin: 30px 150px;
+border: 1px solid lightgray;
+border-radius: 20px;
+width: 80%;
+height: 270px;
+.parent{
+    width: 95%;
+    height: 360px;
+    margin-left: 30px;
+    margin-top: 20px;
+    .history{
+        margin-top: 40px;
+    }
+}
+.title{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+.center{
+    display: flex;
+    align-items: center;
+    justify-content:space-between;
+    margin-top: 50px;
+}
+ `
