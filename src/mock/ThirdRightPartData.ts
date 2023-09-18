8cfdfc3662aa759659f69e2c4dcfb298c73d5674
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-18 10:03:15 +0300

    Mock data file for ThirdRightDataSectoin in the RightSection in profilePage

---
+++ src/mock/ThirdRightPartData.ts
@@ -0,0 +1,10 @@
+export const ThirdRightPartData: {
+    title: string;
+    about: string;
+    link: string;
+  } = {
+    title: "Portfolio",
+    about: "Talent who add portfolios to their profile are more likely to win work. (+20%)",
+    link: "Add a portfolio",
+  };
+  
