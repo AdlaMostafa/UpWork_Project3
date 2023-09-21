68c8802098cd579c42d3bbe33238f7d0f0811cf5
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-21 17:20:53 +0300

    Style for the Hourly Rate Edit that i use it in the Profile Page

---
+++ src/components/pages/ProfilePage/GirdSections/EditPop/HourlyRate/style.ts
@@ -0,0 +1,12 @@
+import { styled } from "@mui/material";
+
+export const StyledDivhourlyrate = styled("div")(({ theme }) => ({
+  textTransform: "none",
+  display: "flex",
+  justifyContent: "space-between",
+
+  color: "black",
+  alignItems: "center",
+  width: "100%",
+  borderBottom: `1px solid gray`,
+}));
