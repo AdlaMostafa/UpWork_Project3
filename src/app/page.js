862cf5a0029cee17e854a937d960e09b683571a4
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-21 21:13:17 +0300

    Adding Page.js code

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,25 @@
-import Image from 'next/image'
 
+import Header from '@/components/organism/Header'
+import SubFooter from '@/components/organism/SubFooter'
+import SupFooter from '@/components/organism/SupFooter'
+import LoginPage from '@/components/pages/Auth/LoginPage/page'
+import SignupPage from '@/components/pages/Auth/SignupPage/page'
+import HomePage from '@/components/pages/HomePage/index'
+import ProfilePage from '../components/pages/ProfilePage'
+import AdditionalSections from '../components/pages/ProfilePage/AdditionalSections'
+import JobPage from '../components/pages/JobPage'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h2>Hi Adola</h2>
+      <Header/>
+      <LoginPage/>
+      <SignupPage/>
+      <HomePage/>
+      <ProfilePage/>
+      <AdditionalSections/>
+      <JobPage/>
+       <SubFooter/>
+      <SupFooter/>
     </main>
   )
 }
