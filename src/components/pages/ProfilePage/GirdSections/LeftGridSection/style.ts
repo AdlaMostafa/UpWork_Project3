c77cc88c517918bfebcb2c40f21b1649e620a3e2
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-21 17:40:38 +0300

    Style for LeftGridSection in Profile Page

---
+++ src/components/pages/ProfilePage/GirdSections/LeftGridSection/style.ts
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+export const StyleParent = styled.div`
+font-family: 'PP Neue Montreal', sans-serif;
+height: 1000px;
+`
+export const StyleOne = styled.div`
+display: flex;
+align-items: center;
+justify-content: space;
+margin:30px 0 0 20px ;
+font-weight: 550;
+`
+export const StyleTwo = styled.div`
+.titleandIcon{
+display: flex;
+align-items: center;
+justify-content: space;
+margin: 30px 0 0 20px;
+font-weight: 550; 
+.icon{
+    margin-left: 30px;
+}
+}
+.workHours{
+    margin: 10px 0 0 20px;
+}
+
+`
+export const StyleThree = styled.div`
+.textandIcons{
+display: flex;
+align-items: center;
+justify-content: space;
+margin: 30px 0 0 20px;
+font-weight: 550;   
+}
+.languages{
+    margin: 10px 0 0 20px;
+.content{
+    display: flex;
+    align-items: center;
+}
+}
+`
+export const StyleFour = styled.div`
+.title{
+margin:30px 0 0 20px;
+font-weight: 550; 
+}
+.iconandText{
+    display: flex;
+    align-items: center;
+    margin: 5px 0 0 20px;
+}
+`
+export const StyleFive = styled.div`
+margin: 30px 0 20px 20px;
+.titleandIcon{
+    display: flex;
+    align-items: center;
+    margin-top: 20px;
+}
+`
