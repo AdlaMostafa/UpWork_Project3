846c97b3f5caeb673f1c8f72e99a1e54cf8febbf
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-21 17:42:42 +0300

    Code for the Fourth Right Part in RightGridSection  in Profile Page

---
+++ src/components/pages/ProfilePage/GirdSections/RightGridSection/FifthRightPart/style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledParent5 = styled.div`
+font-family: 'PP Neue Montreal', sans-serif;
+margin: 20px 25px;
+.title5{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+`
