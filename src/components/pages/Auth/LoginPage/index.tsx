6f3e64ec557d32b984d71300c19738608f7576a3
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-21 15:30:46 +0300

    Update the code for the Login Page and convert it to tsx

--- src/components/pages/Auth/LoginPage/index.tsx
+++ src/components/pages/Auth/LoginPage/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import LoginForm from './LoginForm'
+import LoginForm from './LoginForm/index'
 import Image from 'next/image'
 import Logo from '../../../../../public/assets/SocialMedia/Upwork Logo.png'
 import SubFooter from '@/components/organism/SubFooter'
