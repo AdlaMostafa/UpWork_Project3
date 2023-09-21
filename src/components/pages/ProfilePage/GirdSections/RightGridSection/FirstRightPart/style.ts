2e9ebb0e30e60f76204a9e84eb3389eba24f5c32
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-21 17:45:38 +0300

    Style for the First Right Part in RightGridSection  in Profile Page

---
+++ src/components/pages/ProfilePage/GirdSections/RightGridSection/FirstRightPart/style.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const StyledParent = styled.div`
+font-family: 'PP Neue Montreal', sans-serif;
+margin:-60px 25px 0 25px;
+.titlePart{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}                                               
+.right{
+    display: flex;
+    align-items: center;
+}
+.left{
+    display: flex;
+    align-items: center;
+}
+.about{
+    display: flex;
+}
+.Dialog{
+    color: red;
+button{
+    background:#3897f1;
+}
+}
+`
+export const StyledDialogWrapper = styled.div`
+background-color: red;
+`
