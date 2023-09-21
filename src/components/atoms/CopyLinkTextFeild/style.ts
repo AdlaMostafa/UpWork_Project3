139629016cf2da0e38b09cec27d2c34b87a5fd5b
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-21 15:25:22 +0300

    Style code for  CopyLink that i use it in the Drawer content in Home Page

---
+++ src/components/atoms/CopyLinkTextFeild/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.div`
+button{
+    background: none;
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+}
+`
