70b41c9543c613b49c9fe16b509ff9538807c8be
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-21 15:57:41 +0300

    Code for HomePsge

--- src/components/pages/HomePage/index.tsx
+++ src/components/pages/HomePage/index.tsx
@@ -6,9 +6,11 @@ import RightSection from "./sections/rightSections/index";
 import StyledHomePage, { StyledFooter } from "./style";
 import SuperFooter from "../../../components/organism/SupFooter";
 import SubFooter from "../../../components/organism/SubFooter/index";
+import Header from "@/components/organism/Header";
 const HomePage: React.FC = () => {
   return (
     <div>
+      <Header/>
       <Container>
         <StyledHomePage>
           <LeftSection />
