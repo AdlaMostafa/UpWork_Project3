7ca09f0beff00d6cc45300832e60a5e265b871d0
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-21 17:47:35 +0300

    Style for the Second Right Part in RightGridSection  in Profile Page

---
+++ src/components/pages/ProfilePage/GirdSections/RightGridSection/SecondRightPart/style.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const StyledParent2 = styled.div`
+font-family: 'PP Neue Montreal', sans-serif;
+margin: 20px 25px;
+a{
+   color :green;
+   text-decoration: none;
+   margin-top: 10px;
+}
+a:hover{
+    text-decoration: underline;
+}
+.link{
+    margin-top: 20px;
+}
+`
