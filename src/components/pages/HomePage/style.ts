bb931992b15fe447ac8eccf623d953a3ff02d58e
Adla Mostafa <adlamostafa422004@gmail.com>
2023-10-04 20:02:22 +0300

    Updating the code for the HomePage

--- src/components/pages/HomePage/style.ts
+++ src/components/pages/HomePage/style.ts
@@ -1,16 +1,12 @@
 import styled from "styled-components";
 
-interface StyledHomePageProps {
-}
-
+interface StyledHomePageProps {}
 const StyledHomePage = styled.div<StyledHomePageProps>`
   display: flex;
-  align-items: center;
   justify-content: space-between;
-  /* margin-top: 40px; */
 `;
 export const StyledFooter = styled.div`
-margin-top: 20px;
-`
+  margin-top: 20px;
+`;
 
 export default StyledHomePage;
