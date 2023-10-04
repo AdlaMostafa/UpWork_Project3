a42c98aaeceea343a0879618db76cdcba983a023
Adla Mostafa <adlamostafa422004@gmail.com>
2023-10-04 19:40:12 +0300

    Adding profile => page.js

---
+++ src/app/profile/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ProfilePage from '../../components/pages/ProfilePage'
+const profile = () => {
+  return (
+    <div><ProfilePage/></div>
+  )
+}
+
+export default profile
