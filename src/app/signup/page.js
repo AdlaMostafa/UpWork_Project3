43b42de5d397971e08c20a2a9a40092842c3580f
Adla Mostafa <adlamostafa422004@gmail.com>
2023-10-04 19:41:08 +0300

    Adding signup => page.js

---
+++ src/app/signup/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import SignupPage from '../../components/pages/Auth/SignupPage'
+const signup = () => {
+  return (
+    <div><SignupPage/></div>
+  )
+}
+
+export default signup
