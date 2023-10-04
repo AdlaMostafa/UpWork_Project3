742e20f71a766da07da66915ca2eecae4f22be8f
Adla Mostafa <adlamostafa422004@gmail.com>
2023-10-04 19:44:28 +0300

    Updatin the code for the Search Input by replace the Select component to menu component

--- src/components/atoms/SearchInput/index.tsx
+++ src/components/atoms/SearchInput/index.tsx
@@ -3,52 +3,68 @@ import * as React from 'react';
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
 import Divider from '@mui/material/Divider';
+import { selectData } from '@/mock/SelectComponentInfo';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
-import SelectVariants from '../SelectComponent'; 
-import { selectData } from '@/mock/SelectComponentInfo';
 import { StyledSearch } from './style';
 
-interface SearchComponentProps{}
+import { makeStyles } from '@mui/styles';
 
-const SearchComponent:React.FC<SearchComponentProps> = () => {
-  const [selectedValue4,setSelectedValue4]=React.useState<string>('');
+interface SearchComponentProps {}
+
+const useStyles = makeStyles({
+  select1: {
+    color: 'green',
+    border: 'none',
+    marginRight: '5px',
+    height: '30px',
+    marginTop:'20px',
+    fontSize:'18px',
+  },
+});
+
+const SearchComponent: React.FC<SearchComponentProps> = () => {
+  const classes = useStyles(); 
+
+  const [selectedValue4, setSelectedValue4] = React.useState<string>('');
   const handleSelectChange4 = (event: React.ChangeEvent<{ value: string }>) => {
     setSelectedValue4(event.target.value);
   };
 
   return (
-    <Paper 
+    <Paper
       component="form"
-      sx={{ 
-       p: '1px 1px',
-       display: 'flex', 
-       alignItems: 'center', 
-       width: 400 ,
-       marginLeft:'262px',
-       marginTop:'20px',
-       border:'1px solid lightgray',
-       height:'35px',
-       backgroundColor:'white',
-       borderRadius:'40px',
+      sx={{
+        p: '1px 1px',
+        display: 'flex',
+        alignItems: 'center',
+        width: 400,
+        marginLeft: '262px',
+        marginTop: '20px',
+        border: '1px solid lightgray',
+        height: '35px',
+        backgroundColor: 'white',
+        borderRadius: '40px',
       }}
     >
       <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-        <SearchIcon/>
+        <SearchIcon />
       </IconButton>
-      <InputBase
-        sx={{ ml: 1, flex: 1 }}
-        placeholder="Search"
-      />
-      
+      <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Search" />
+
       <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
       <StyledSearch>
-      <SelectVariants
-          label={selectData[3].label}
-          options={selectData[3].options}
+        <select
           value={selectedValue4}
           onChange={handleSelectChange4}
-        />
+          className={classes.select1}
+        >
+          {selectData[3].options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
       </StyledSearch>
     </Paper>
   );
